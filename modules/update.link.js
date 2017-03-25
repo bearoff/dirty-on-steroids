9a4662ba233a5e43e2241deefba4742c58556ac5
bear <offbear.me@gmail.com>
2017-03-25 15:26:37 +0200

    [UPDATE LINK] Setting link to bearoff fork

--- modules/update.link.js
+++ modules/update.link.js
@@ -6,13 +6,13 @@ d3.addModule(
 	name: 'Линк на последнюю версию',
 	author: 'crimaniak',
 	config: {
-		checkUpdates:{type:'checkbox', value: 0, caption: 'проверять наличие новых версий', description: 'Если эта опция активна, время от времени сервис-пак проверяет наличие обновлений и сообщает о них, а также спрашивает разрешение на обновление.'},
+		checkUpdates:{type:'checkbox', value: 0, caption: 'проверять наличие новых версий (bearoff fork)', description: 'Если эта опция активна, время от времени сервис-пак проверяет наличие обновлений и сообщает о них, а также спрашивает разрешение на обновление.'},
 		ver:{type:'html', value:'Текущая версия: '+d3.buildNumber+' '+d3.buildMode+' от '+d3.buildTime},
-		link:{type:'html',value:'<a href="https://github.com/crimaniak/dirty-on-steroids/raw/master/result/d3.user.js">Установить последнюю версию скрипта</a>'}
+		link:{type:'html',value:'<a style="text-decoration: underline;" href="https://github.com/bearoff/dirty-on-steroids/raw/master/result/d3.user.js">Установить последнюю версию скрипта (bearoff fork)</a>'}
 	},
 	lastCheckName: 'lastUpdateCheck',
 	interval: 3600000*6,
-	jsonLink: 'https://api.github.com/repos/crimaniak/dirty-on-steroids/contents/result/pack.version.json?callback=checkUpdate',
+	jsonLink: 'https://api.github.com/repos/bearoff/dirty-on-steroids/contents/result/pack.version.json?callback=checkUpdate',
 	run: function(){
 		if(!this.config.checkUpdates.value) return;
 		var lastCheck = d3.storage.get(this.lastCheckName, {time: 0, result: ''});
@@ -58,8 +58,8 @@ d3.addModule(
 		}
 		d3.storage.set(this.lastCheckName, {time:now, result:buildTime});
 		if(buildTime > d3.buildTime &&
-			confirm('Доступна новая версия сервис-пака. Попробуем поставить?')) {
-			document.location = 'https://github.com/crimaniak/dirty-on-steroids/raw/master/result/d3.user.js';
+			confirm('Доступна новая версия сервис-пака (bearoff fork). Попробуем поставить?')) {
+			document.location = 'https://github.com/bearoff/dirty-on-steroids/raw/master/result/d3.user.js';
 		}
 	}
 
